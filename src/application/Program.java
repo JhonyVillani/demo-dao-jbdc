b9336bb1b5926a94908aae206af01b328c3be377
Jhony <jhony.v.m@hotmail.com>
2019-10-03 13:33:12 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,6 +41,10 @@ public class Program {
 		seller.setName("Pedro Villani");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
+		
+		System.out.println("\n=== TEST 6: seller delete ===");
+		sellerDao.deleteById(9);
+		System.out.println("Delete completed");
 	}
 
 }
